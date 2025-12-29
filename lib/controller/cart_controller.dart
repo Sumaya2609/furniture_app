a43e685ae5f35acb350dfac9a437eab25deac8a6
Sumaya2609 <erasumayasultana@gmail.com>
2025-12-30 01:35:02 +0600

    cart_controller.dart

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -1,25 +1,44 @@
 import 'package:get/get.dart';
 import '../models/cart_item.dart';
+import '../database/cart_db.dart';
 
 class CartController extends GetxController {
   var cartItems = <CartItem>[].obs;
 
-  void addToCart(CartItem item) {
-    final index =
-    cartItems.indexWhere((element) => element.name == item.name);
+  @override
+  void onInit() {
+    super.onInit();
+    loadCart();
+  }
+
+  void loadCart() async {
+    cartItems.value = await CartDB.getCart();
+  }
+
+  void addToCart(CartItem item) async {
+    final index = cartItems.indexWhere((e) => e.name == item.name);
 
     if (index >= 0) {
       cartItems[index].quantity += item.quantity;
-      cartItems.refresh();
     } else {
       cartItems.add(item);
     }
+    cartItems.refresh();
+    await CartDB.insertCart(item);
+  }
+
+  void removeFromCart(String name) async {
+    await CartDB.deleteCart(name);
+    loadCart();
+  }
+
+  void clearCart() async {
+    await CartDB.clearCart();
+    cartItems.clear();
   }
 
   double get totalPrice => cartItems.fold(
-    0,
-        (sum, item) => sum + (item.price * item.quantity),
-  );
-
-
+        0,
+        (sum, item) => sum + item.price * item.quantity,
+      );
 }
